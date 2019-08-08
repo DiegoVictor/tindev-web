0f4c021af14b691e839815e2d9925ef00d6f73e7
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-08-08 18:53:55 -0300

    Create loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Loading.css';
+
+export default function Loading() {
+  return (<div class="lds-roller">
+    <div></div><div></div><div></div><div></div>
+    <div></div><div></div><div></div><div></div>
+  </div>);
+}
