563dc7743a08604b7f58852ed30a9300ae45b668
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-08-10 13:05:18 -0300

    Fix property name

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Loading.css';
 
 export default function Loading() {
-  return (<div class="lds-roller">
+  return (<div className="lds-roller">
     <div></div><div></div><div></div><div></div>
     <div></div><div></div><div></div><div></div>
   </div>);
