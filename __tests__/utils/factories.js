8244c098df19e89c662331b6a0d477619458155f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-30 20:44:53 -0300

    Use a simple object

--- __tests__/utils/factories.js
+++ __tests__/utils/factories.js
@@ -1,13 +1,15 @@
 import { factory } from 'factory-girl';
 import faker from 'faker';
 
-class Developer {}
-
-factory.define('Developer', Developer, {
-  _id: faker.random.uuid,
-  name: faker.name.firstName,
-  bio: faker.lorem.paragraph,
-  avatar: faker.image.imageUrl,
-});
+factory.define(
+  'Developer',
+  {},
+  {
+    _id: faker.random.uuid,
+    name: faker.name.firstName,
+    bio: faker.lorem.paragraph,
+    avatar: faker.image.imageUrl,
+  }
+);
 
 export default factory;
