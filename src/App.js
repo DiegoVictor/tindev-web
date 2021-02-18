44eae5b0ca5494e30390d0f273a6ea62a3f7f4ec
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-18 18:45:33 -0300

    Configure toastify container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+
 import Theme, { Container } from '~/styles/theme';
 export default () => {
   return (
     <Container>
       <Theme />
+      <ToastContainer />
     </Container>
   );
 };
