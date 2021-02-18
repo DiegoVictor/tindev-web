00b35d2b11651e27e2840f584c4b9ee6c401c8ad
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-18 18:45:41 -0300

    Import routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 
 import Theme, { Container } from '~/styles/theme';
+import Routes from '~/routes';
+
 export default () => {
   return (
     <Container>
       <Theme />
       <ToastContainer />
+      <Routes />
     </Container>
   );
 };
