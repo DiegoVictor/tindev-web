dd392ba3807efa725225d412b85e65e2e45299e7
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-20 23:29:51 -0300

    Store api token after login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,16 @@
 import React, { useContext, useCallback } from 'react';
 import { Form, Input } from '@rocketseat/unform';
+import { toast } from 'react-toastify';
 
 import Logo from '~/assets/logo.svg';
 import { UserContext } from '~/contexts/User';
-import api from '~/services/api';
+import api, { setAuthorization } from '~/services/api';
 import history from '~/services/history';
 import { Container, Button } from './styles';
 
 export default () => {
   const user = useContext(UserContext);
+
   const handleSubmit = useCallback(
     async ({ username }) => {
       try {
@@ -23,6 +25,8 @@ export default () => {
         );
         user.id = data.developer._id;
         user.token = data.token;
+
+        setAuthorization(data.token);
 
         history.push('/developers');
       } catch (err) {
